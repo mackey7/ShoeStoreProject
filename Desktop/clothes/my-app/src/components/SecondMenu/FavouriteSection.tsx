27ee06c36299cdb141fde7a20c9f88651305d2a5
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-07-31 13:35:31 +0200

    add content to FavouriteSection file

--- Desktop/clothes/my-app/src/components/SecondMenu/FavouriteSection.tsx
+++ Desktop/clothes/my-app/src/components/SecondMenu/FavouriteSection.tsx
@@ -2,16 +2,26 @@ import React from 'react'
 
 import styled from 'styled-components'
 
-const Icon = styled.div`
+const Icon = styled.i`
+font-size:24px;
 `
-
+const Span = styled.span`
+background:#efefef;
+border-radius:6px;
+padding:2px;
+font-size:20px;
+text-align:center;
+`
 
 
 
 
 const FavouriteSection: React.SFC = () => {
     return (
-        <Icon>Favourite</Icon>
+        <div>
+            <Icon className="far fa-star"></Icon>
+            <Span> 30</Span>
+        </div>
     )
 }
 
