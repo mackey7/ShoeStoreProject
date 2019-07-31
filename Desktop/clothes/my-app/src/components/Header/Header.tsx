49496afa2ece3ec78f9179cf8cd49128edf25925
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-07-31 14:49:08 +0200

    create image and header file

---
+++ Desktop/clothes/my-app/src/components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from './Image'
+import styled from 'styled-components'
+
+
+interface Iprops {
+
+}
+interface IState {
+
+}
+class Header extends React.Component<Iprops, IState> {
+
+    render() {
+        return (
+            <div>
+
+                <Image />
+
+            </div>
+        )
+    }
+
+}
+
+
+
+export default Header
---
+++ Desktop/clothes/my-app/src/components/Header/Image.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+const Image: React.SFC = () => {
+    return (
+        <p>IMAGE</p>
+    )
+}
+
+export default Image
