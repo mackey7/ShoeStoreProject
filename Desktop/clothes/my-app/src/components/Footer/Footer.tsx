7cf04e91c87a25a1f4ff207034777d19b596effc
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-07-31 18:13:00 +0200

    create styles for footer component

--- Desktop/clothes/my-app/src/components/Footer/Footer.tsx
+++ Desktop/clothes/my-app/src/components/Footer/Footer.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 import Styled from 'styled-components'
 import LeftSection from './LeftSection';
 import RightSection from './RightSection';
-const FooterWrapper = Styled.footer``
+const FooterWrapper = Styled.footer`
+display:flex;
+justify-content: space-around;
+background:#2e2e2e;
+padding:20px 0px;
+color:#979797;
+`
 
 const Footer: React.SFC = () => {
     return (
