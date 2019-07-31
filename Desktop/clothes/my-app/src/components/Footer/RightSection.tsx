ce008eeb62e2952737b33a391f84829a899b1c1c
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-07-31 18:28:44 +0200

    add content to RightSection component

--- Desktop/clothes/my-app/src/components/Footer/RightSection.tsx
+++ Desktop/clothes/my-app/src/components/Footer/RightSection.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Styled from 'styled-components'
 
+const UL = Styled.ul`
+display:flex;
+`
+const LI = Styled.li`
+list-style:none
 
+`
+const LINK = Styled(Link)`
+color:#979797;
+text-decoration:none;
+padding:0px 10px;
+&:hover{
+    color:#fff;
+}
+`
 const RightSection: React.SFC = () => {
     return (
-        <section>
-            RightSection
-        </section>
+
+        <UL>
+            <LI> <LINK to="/Home">Home</LINK></LI>
+            <LI> <LINK to="/Support">Support</LINK></LI>
+            <LI> <LINK to="/Terms">Terms and Conditions</LINK></LI>
+            <LI> <LINK to="/Faqs">Faqs</LINK></LI>
+            <LI> <LINK to="/Contact">Contact us</LINK></LI>
+        </UL>
+
     )
 }
 
