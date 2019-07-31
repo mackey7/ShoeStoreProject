83548e1e305f49ab44531fa51af13714bfbabffe
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-07-31 19:45:27 +0200

    add content

--- Desktop/clothes/my-app/src/components/Widgets/TwitterWidget.tsx
+++ Desktop/clothes/my-app/src/components/Widgets/TwitterWidget.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
-
+import WidgetTitle from './WidgetTitle'
 
 const TwitterWidget: React.SFC = () => {
     return (
         <section>
-            TwitterWidget
+            <WidgetTitle title="twitter widget" />
+            <section>
+                <p>
+                    <span>@ericfustero</span>
+                    Why thank you. Your work looks awasome by the way!
+                </p>
+                <span> 19 days ago</span>
+            </section>
+            <section>
+                <p>
+                    <span>@roymarvelous</span>
+                    You can seek a refund through. TF if is it not as advertised - but it is :)
+                </p>
+                <span> 21 days ago</span>
+            </section>
         </section >
     )
 }
