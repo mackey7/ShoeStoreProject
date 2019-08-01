a714bbdad221b458ac995eedd72b378878bb48fd
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-08-01 21:30:30 +0200

    add style

--- Desktop/clothes/my-app/src/components/SocialMediaSection/NewsletterForm.tsx
+++ Desktop/clothes/my-app/src/components/SocialMediaSection/NewsletterForm.tsx
@@ -1,14 +1,32 @@
 import React from 'react'
+import Styled from 'styled-components'
 
+const Form = Styled.form`
+input[type="text"]{
+    border:0px;
+    background:#2e2e2e;
+    padding:10px;
+     color:#7d7d7d;
+}
+input[type="button"]{
+    border:0;
+    padding:8px;
+      background:#7d7d7d;
+      color:#2e2e2e;
+      font-size:13px;
+      font-weight:900;
+      cursor:pointer;
+}
 
+`
 
 const NewsletterForm: React.SFC = () => {
     return (
-        <form action="" method="post">
+        <Form action="" method="post">
             <input type="text" placeholder="Enter email for newsletter" />
             <input type="button" value=">" />
 
-        </form>
+        </Form>
     )
 }
 
--- Desktop/clothes/my-app/src/components/SocialMediaSection/SocialMediaSection.tsx
+++ Desktop/clothes/my-app/src/components/SocialMediaSection/SocialMediaSection.tsx
@@ -1,14 +1,40 @@
 import React from 'react'
 import SocialIcons from './SocialIcons'
 import NewsletterForm from './NewsletterForm'
+import Styled from 'styled-components'
+
+const SectionWrapper = Styled.section`
+background-color:#242424;
+border-top:1px solid black;
+padding:20px;
+
+`
+const Section = Styled.section`
+display:flex;
+flex-direction:column;
+justify-content:center;
+align-items:center;
+align-content:space-around;
+width:90%;
+
+@media only screen and (min-width: 600px) {
+  display:flex;
+  flex-direction:row;
+  justify-content:space-between;
+
+width:80%;
+margin:0 auto;
+}
+`
 
 const SocialMediaSection: React.SFC = () => {
     return (
-        <div>
-
-            <NewsletterForm />
-            <SocialIcons />
-        </div>
+        <SectionWrapper>
+            <Section>
+                <NewsletterForm />
+                <SocialIcons />
+            </Section >
+        </SectionWrapper>
     )
 }
 
