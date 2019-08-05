223c42036c3d80a112620ebab39277927465bdda
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-08-05 20:58:04 +0200

    add style

--- Desktop/clothes/my-app/src/components/HomeCategory/HomeCategory.tsx
+++ Desktop/clothes/my-app/src/components/HomeCategory/HomeCategory.tsx
@@ -2,13 +2,40 @@ import React from 'react'
 import Styled from 'styled-components'
 
 const CategoryWrapper = Styled.section`
-
+padding: 0px 20px;
 background:#ebe7df;
 `
-const Category = Styled.section``
-const CategoryHeader = Styled.div``
-const ProductsItems = Styled.div``
+const Category = Styled.section`
+ margin:0 auto;
+@media screen and (min-width: 600px) {
+ width:80%; 
 
+}
+`
+const CategoryHeader = Styled.div`
+display:flex;
+flex-direction:column;
+@media screen and (min-width: 600px) {
+  display:flex;
+flex-direction:row;
+justify-content:space-between;
+}
+`
+const ProductsItems = Styled.div``
+const Button = Styled.input`
+background:inherit;
+border:1px solid #bcb5a7;
+padding:5px 20px;
+color:#bcb5a7;
+font-weight:700;
+&:hover{
+ color:#000;;   
+ background:#bcb5a7;
+ cursor: pointer;
+}
+
+
+`
 interface IProps {
     title: string
 }
@@ -18,11 +45,9 @@ const HomeCategory: React.SFC<IProps> = ({ title }) => {
         <CategoryWrapper>
             <Category>
                 < CategoryHeader >
-                    <h2>
-                        {title}
-                    </h2>
+                    <h2> {title}</h2>
 
-                    <input type="button" value="Show All" />
+                    <Button type="button" value="Show All" />
                 </CategoryHeader>
 
                 <ProductsItems>
