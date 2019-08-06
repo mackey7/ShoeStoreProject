e7eaa2ed0583c009ae6eed96125851695be7f234
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-08-06 21:28:53 +0200

    add styles

--- Desktop/clothes/my-app/src/components/SepecialsCategory/CategoryItem.tsx
+++ Desktop/clothes/my-app/src/components/SepecialsCategory/CategoryItem.tsx
@@ -1,4 +1,28 @@
 import React from 'react'
+import Styled from 'styled-components'
+
+const Image = Styled.figure`
+padding:20px 10px;
+position:realtive;
+img{
+width:100%;
+height:200px;
+
+}
+figcaption{
+position:absolute;
+top:50%;
+text-align: center; 
+max-width:100%;
+color:#fff;
+background:rgb(0.00,0.00,0.00, 0.5);
+padding:10px 20px;
+text-transform:uppercase;
+font-weight:400;
+}
+}
+`
+
 interface IProps {
     alt: string;
     src: string;
@@ -6,11 +30,11 @@ interface IProps {
 }
 const CategoryItem: React.SFC<IProps> = ({ alt, src, name }) => {
     return (
-        <figure>
+        <Image>
             <img src={src}
                 alt={alt} />
             <figcaption>{name}</figcaption>
-        </figure>
+        </Image>
     )
 }
 
--- Desktop/clothes/my-app/src/components/SepecialsCategory/SpecialsCategory.tsx
+++ Desktop/clothes/my-app/src/components/SepecialsCategory/SpecialsCategory.tsx
@@ -1,12 +1,32 @@
 import React from 'react'
+import Styled from 'styled-components'
+
+const Wrapper = Styled.section`
+background:#242424;
+`
+const Section = Styled.section`
+width:90%;
+margin:0 auto;
+
+
+@media only screen and (min-width: 600px) {
+  display:flex;
+  flex-direction:row;
+  width:80%;
+}
+
+`
+
 import CategoryItem from './CategoryItem'
 const SpecialsCategory: React.SFC = () => {
     return (
-        <div>
-            <CategoryItem alt="alt" src="https://m.media-amazon.com/images/G/01/6pm/landing/2019/Shoes/June/Mens_Boat_Shoes._CB442712240_.jpg" name="on sale" />
-            <CategoryItem alt="alt" src="https://m.media-amazon.com/images/G/01/6pm/landing/2019/Shoes/June/Mens_Boat_Shoes._CB442712240_.jpg" name="on sale" />
-            <CategoryItem alt="alt" src="https://m.media-amazon.com/images/G/01/6pm/landing/2019/Shoes/June/Mens_Boat_Shoes._CB442712240_.jpg" name="on sale" />
-        </div>
+        <Wrapper>
+            <Section >
+                <CategoryItem alt="alt" src="https://m.media-amazon.com/images/G/01/6pm/landing/2019/Shoes/June/Mens_Boat_Shoes._CB442712240_.jpg" name="on sale" />
+                <CategoryItem alt="alt" src="https://m.media-amazon.com/images/G/01/6pm/landing/2019/Shoes/June/Mens_Boat_Shoes._CB442712240_.jpg" name="on sale" />
+                <CategoryItem alt="alt" src="https://m.media-amazon.com/images/G/01/6pm/landing/2019/Shoes/June/Mens_Boat_Shoes._CB442712240_.jpg" name="on sale" />
+            </Section >
+        </Wrapper>
     )
 }
 
