a017ef70da4d65d8ad0a8b35c9f80f65ff6557aa
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-08-06 23:21:44 +0200

    add styles

--- Desktop/clothes/my-app/src/components/Carousel/CarouselDots.tsx
+++ Desktop/clothes/my-app/src/components/Carousel/CarouselDots.tsx
@@ -1,17 +1,41 @@
 import React from 'react'
 import Dot from './Dot'
+import Styled from 'styled-components'
 
+
+const CarouselDotsWrapper = Styled.section`
+background:#2e2e2e;
+`
+const CarouselDotsSection = Styled.section`
+margin:0 auto;
+width:90%;
+display:flex;
+justify-content:space-between;
+padding-bottom:4px 0px 30px 0px;
+
+@media only screen and (min-width: 300px) {
+ 
+}
+@media only screen and (min-width: 600px) {
+ 
+}
+@media only screen and (min-width: 900px) {
+width:80%;
+
+}
+`
 
 
 const CarouselDots: React.SFC = () => {
     return (
-
-        <section>
-            <Dot DotTitle="Pink Shoes" DotDesc="Now %145.99" />
-            <Dot DotTitle="Pink Shoes" DotDesc="Now %145.99" />
-            <Dot DotTitle="Pink Shoes" DotDesc="Now %145.99" />
-            <Dot DotTitle="Pink Shoes" DotDesc="Now %145.99" />
-        </section>
+        <CarouselDotsWrapper>
+            <CarouselDotsSection>
+                <Dot DotTitle="Pink Shoes" DotDesc="Now %145.99" />
+                <Dot DotTitle="Pink Shoes" DotDesc="Now %145.99" />
+                <Dot DotTitle="Pink Shoes" DotDesc="Now %145.99" />
+                <Dot DotTitle="Pink Shoes" DotDesc="Now %145.99" />
+            </CarouselDotsSection>
+        </CarouselDotsWrapper>
     )
 }
 
--- Desktop/clothes/my-app/src/components/Carousel/Dot.tsx
+++ Desktop/clothes/my-app/src/components/Carousel/Dot.tsx
@@ -1,4 +1,69 @@
 import React from 'react'
+import Styled from 'styled-components'
+
+
+const DotWrapper = Styled.div`
+margin-left:10px;
+
+`
+
+
+const DotInfo = Styled.div`
+ display:none;
+ color:#fff;
+ 
+@media only screen and (min-width: 300px) {
+
+}
+@media only screen and (min-width: 600px) {
+ 
+}
+@media only screen and (min-width: 900px) {
+display:flex;
+flex-direction:column;
+
+}
+`
+const SlideWrapper = Styled.div`
+height:25px;
+width:25px;
+border-radius:50%;
+background:#000
+display:flex;
+@media only screen and (min-width: 300px) {
+ 
+}
+@media only screen and (min-width: 600px) {
+ 
+}
+@media only screen and (min-width: 900px) {
+ 
+}
+
+`
+const Slide = Styled.div`
+align-self:center;
+margin:0 auto;
+
+height:15px;
+width:15px;
+border-radius:50%;
+background:#57c5a0
+
+@media only screen and (min-width: 300px) {
+ 
+}
+@media only screen and (min-width: 600px) {
+ 
+}
+@media only screen and (min-width: 900px) {
+ 
+}
+
+`
+
+
+
 
 interface Iprops {
     DotTitle: string;
@@ -7,15 +72,15 @@ interface Iprops {
 
 const Dot: React.SFC<Iprops> = ({ DotTitle, DotDesc }) => {
     return (
-        <div>
-            <div>
-                <span></span>
-            </div>
-            <div>
+        <DotWrapper>
+            <SlideWrapper>
+                <Slide></Slide>
+            </SlideWrapper>
+            <DotInfo>
                 <p>{DotTitle}</p>
                 <p>{DotDesc}</p>
-            </div>
-        </div>
+            </DotInfo>
+        </DotWrapper>
     )
 }
 
