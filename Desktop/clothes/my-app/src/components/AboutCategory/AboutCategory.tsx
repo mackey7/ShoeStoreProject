365429bbfb550efeb6b75e675ece51fb24702156
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-08-22 22:43:05 +0200

    create AboutCategory component

---
+++ Desktop/clothes/my-app/src/components/AboutCategory/AboutCategory.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Styled from 'styled-components'
+
+
+
+
+const AboutCategory: React.SFC = () => {
+
+    return (
+        <div>
+            AboutCategory
+        </div>
+    )
+}
+
+export default AboutCategory
