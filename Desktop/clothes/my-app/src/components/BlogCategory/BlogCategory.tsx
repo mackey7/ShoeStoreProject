9ec3417befedf7c6af85377893a505cd55513da0
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-08-22 23:34:44 +0200

    create BlogCategory component

---
+++ Desktop/clothes/my-app/src/components/BlogCategory/BlogCategory.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Styled from 'styled-components'
+
+
+const BlogCategory: React.SFC = () => {
+
+    return (
+        <div>
+            BlogCategory
+        </div>
+
+    )
+}
+
+export default BlogCategory
