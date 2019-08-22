87abdd83e14067a81fab55abfacc43e7252a7200
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-08-22 23:42:54 +0200

    create ContactCategory  component

---
+++ Desktop/clothes/my-app/src/components/ContactCategory/ContactCategory.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Styled from 'styled-components'
+
+
+const ContactCategory: React.SFC = () => {
+
+    return (
+        <div>
+            ContactCategory
+        </div>
+
+    )
+}
+
+export default ContactCategory
