9647973a8dd978575693825be9014373bec71c6c
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-08-22 23:36:02 +0200

    connect blog container with BlogCategory component

--- Desktop/clothes/my-app/src/containers/Blog/Blog.tsx
+++ Desktop/clothes/my-app/src/containers/Blog/Blog.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import BlogCategory from '../../components/BlogCategory/BlogCategory'
 interface Iprops {
 
 }
@@ -11,7 +11,7 @@ class Blog extends React.Component<Iprops, IState> {
     render() {
         return (
             <div>
-                Blog
+                <BlogCategory />
             </div>
         )
     }
