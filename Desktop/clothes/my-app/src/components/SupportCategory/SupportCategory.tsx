8df9142a9e1a114ce83659bedbab4857e2298f70
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-08-23 19:41:39 +0200

    create SupportCategory component

---
+++ Desktop/clothes/my-app/src/components/SupportCategory/SupportCategory.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Styled from 'styled-components'
+
+
+const SupportCategory: React.SFC = () => {
+
+    return (
+        <div>SupportCategory</div>
+    )
+}
+
+export default SupportCategory
