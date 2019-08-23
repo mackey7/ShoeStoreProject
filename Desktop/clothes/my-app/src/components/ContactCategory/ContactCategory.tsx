226f112e4160e4152feea1db2d0651ae153c72c2
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-08-23 19:36:48 +0200

    add from

--- Desktop/clothes/my-app/src/components/ContactCategory/ContactCategory.tsx
+++ Desktop/clothes/my-app/src/components/ContactCategory/ContactCategory.tsx
@@ -2,12 +2,39 @@ import React from 'react'
 import Styled from 'styled-components'
 
 
+const ContactCategorySection = Styled.section`
+background:#ebe7df;
+`
+const ContactCategoryWrapper = Styled.section`
+width:80%;
+margin:auto;
+`
+const Form = Styled.form`
+display:flex;
+flex-direction:column;
+padding:20px 0;
+`
+
+const H2 = Styled.h2`
+text-align:center;
+padding:20px 0;
+`
 const ContactCategory: React.SFC = () => {
 
     return (
-        <div>
-            ContactCategory
-        </div>
+        <ContactCategorySection>
+            <ContactCategoryWrapper>
+                <H2>Contact</H2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos natus atque eius nesciunt vitae quas architecto a ut at neque eligendi impedit, magnam nisi sit ducimus nihil delectus cum dolorem minima inventore sed similique animi. Sequi, rerum ipsam! Commodi esse ratione eum dolores sint voluptatem totam eius doloribus officia praesentium?</p>
+
+                <Form>
+                    <input type="text" />
+                    <input type="text" />
+                    <textarea name="" id="" ></textarea>
+                    <input type="button" value="Send" />
+                </Form>
+            </ContactCategoryWrapper>
+        </ContactCategorySection>
 
     )
 }
