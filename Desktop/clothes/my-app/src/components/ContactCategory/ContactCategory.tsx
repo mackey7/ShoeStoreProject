974a58b3b5bc232d698c7976846b812506774748
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-08-25 23:18:42 +0200

    add formik

--- Desktop/clothes/my-app/src/components/ContactCategory/ContactCategory.tsx
+++ Desktop/clothes/my-app/src/components/ContactCategory/ContactCategory.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Styled from 'styled-components'
+import { Formik } from 'formik'
 
 
 const ContactCategorySection = Styled.section`
@@ -13,13 +14,29 @@ const Form = Styled.form`
 display:flex;
 flex-direction:column;
 padding:20px 0;
+
+width:700px;
+
+input, textarea{
+    margin:20px;
+    width:100%;
+}
+
 `
 
 const H2 = Styled.h2`
 text-align:center;
 padding:20px 0;
 `
-const ContactCategory: React.SFC = () => {
+
+interface FormValues {
+    email: string,
+    subject: string,
+    content: string
+}
+
+
+const ContactCategory: React.SFC<{}> = () => {
 
     return (
         <ContactCategorySection>
@@ -27,12 +44,34 @@ const ContactCategory: React.SFC = () => {
                 <H2>Contact</H2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos natus atque eius nesciunt vitae quas architecto a ut at neque eligendi impedit, magnam nisi sit ducimus nihil delectus cum dolorem minima inventore sed similique animi. Sequi, rerum ipsam! Commodi esse ratione eum dolores sint voluptatem totam eius doloribus officia praesentium?</p>
 
-                <Form>
-                    <input type="text" />
-                    <input type="text" />
-                    <textarea name="" id="" ></textarea>
-                    <input type="button" value="Send" />
-                </Form>
+
+                <Formik
+
+                    initialValues={{ email: '', subject: "", content: "" }}
+                    onSubmit={values => {
+                        console.log(values);
+                    }}
+
+                    render={({
+                        values,
+                        errors,
+                        handleChange,
+                        handleSubmit
+
+
+
+                    }) => (
+                            <Form onSubmit={handleSubmit}>
+                                <input type="text" name="email" onChange={handleChange} value={values.email} />
+                                <input type="text" name="subject" onChange={handleChange} value={values.subject} />
+                                <textarea name="content" onChange={handleChange} value={values.content} ></textarea>
+                                <input type="submit" value="Send" />
+                            </Form>
+                        )
+
+                    }
+                />
+
             </ContactCategoryWrapper>
         </ContactCategorySection>
 
