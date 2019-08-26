131d7670194ff07df6b1849aec4614f9f8db6431
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-08-27 00:02:32 +0200

    create RegisterPage container

---
+++ Desktop/clothes/my-app/src/containers/RegisterPage/RegisterPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+
+const RegisterPage: React.SFC = () => {
+
+    return (
+        <div>
+            RegisterPage
+        </div>
+    )
+}
+
+export default RegisterPage
