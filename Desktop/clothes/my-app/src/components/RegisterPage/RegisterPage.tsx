173cb6c3346ac04275e290029e87f80b746d7c30
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-08-27 00:05:41 +0200

    create Register Page Component

---
+++ Desktop/clothes/my-app/src/components/RegisterPage/RegisterPage.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import Styled from 'styled-components'
+import { Formik } from 'formik'
+
+const LoginPagesection = Styled.section`
+background:#ebe7df;
+width:100%;
+height:100%;
+display: flex;
+  align-items: center;
+  justify-content: center;
+`
+const LoginPageWrapper = Styled.section`
+width:300px;
+border:2px solid #9a9a9a;
+padding:20px;
+margin:100px 40px;
+background:#f6f4f1;
+`
+const H2 = Styled.h2`
+text-align:center;
+font-size:35px;
+padding-bottom:20px;
+font-weight:600;
+
+`
+
+const Form = Styled.form`
+display:flex;
+flex-direction:column;
+`
+const Input = Styled.input`
+padding:10px;
+margin:5px 0px;
+`
+const Button = Styled.input`
+margin-top:5px ;
+padding:10px;
+
+
+
+
+font-size:20px;
+background:#7db71e;
+border:2px solid #c5c9bd;
+cursor:pointer;
+color:#fff;
+font-weight:700;
+&:hover{
+    background:#08b438;
+    border:2px solid #7db71e;
+}
+
+`
+
+
+const RegisterPageComponent: React.SFC = () => {
+
+    return (
+        <div>
+            RegisterPageComponent
+        </div>
+    )
+}
+
+export default RegisterPageComponent
