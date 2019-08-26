1d61fc99f9e808bbf73835d9c663e57d9728b763
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-08-26 21:33:15 +0200

    add styles

--- Desktop/clothes/my-app/src/components/SupportCategory/SupportCategory.tsx
+++ Desktop/clothes/my-app/src/components/SupportCategory/SupportCategory.tsx
@@ -10,63 +10,94 @@ const SupportCategoryWrapper = Styled.div`
 width:80%;
 margin:auto;
 background: #f5f2ec;
+padding:30px;
+`
+const P = Styled.p`
+margin:40px 0px;
+`
 
+const H2 = Styled.h2`
+text-align:center;
+margin:0px 0px 20px 0px;
+`
+const OL = Styled.ol`
+margin: 20px 0px  0px 40px;
+`
+const UL = Styled.ol`
+margin: 20px 0px  0px 40px;
+`
+
+const LI = Styled.li`
+margin-bottom:5px;
+font-weight:500;
+`
+
+const ListTitle = Styled.h3`
+padding-bottom:10px;
+margin-left:-30px;
 `
 
 const SupportCategory: React.SFC = () => {
-    const P = Styled.p``
+
     return (
         <SupportCategorySection>
 
             <SupportCategoryWrapper>
 
-                <h2>Support</h2>
+                <H2>Support</H2>
 
                 <P>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, ullam quae, ipsum eius eos dolorem sequi rerum id blanditiis distinctio molestias corrupti sint debitis consectetur fuga mollitia dolorum! Alias culpa a ratione reprehenderit, qui hic iste ipsum ducimus saepe fugit maxime dolores! Blanditiis consequuntur tempore dolore doloribus ullam maiores expedita pariatur itaque, mollitia cum quae velit obcaecati! Aperiam, debitis quibusdam? Facere accusantium cupiditate, possimus omnis voluptate nihil, quasi laborum voluptas ratione totam reiciendis qui? Nesciunt, nulla fugiat qui molestiae iusto placeat dolor eos tempora numquam quidem, eveniet voluptas ipsa! Natus excepturi assumenda quibusdam blanditiis animi tempore, consectetur officia rem ipsa alias velit odit iste impedit adipisci similique possimus officiis cumque soluta qui corporis quo dolores veritatis? Quae perferendis consequuntur quo nemo. Esse, nisi veniam rem non modi maxime aliquid accusamus omnis numquam laboriosam eos quisquam dolores inventore pariatur laborum possimus minima dignissimos incidunt expedita ullam ratione eveniet. Eos eveniet dignissimos tempora beatae nostrum, dolorem sequi laudantium. Omnis vitae, harum accusantium quod mollitia numquam incidunt labore, veniam recusandae vero aliquid impedit perferendis optio sapiente eius dignissimos dicta quam ipsum architecto quae esse cupiditate voluptas provident? Aperiam consequatur minima nostrum perspiciatis? Ratione magnam quam nulla, sint sequi maxime numquam itaque molestiae magni?
                 </P>
 
-                <ol>
-                    <li>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore in cum architecto illo similique commodi tenetur perspiciatis dignissimos eligendi non!
+                <OL>
+                    <ListTitle> Lorem ipsum dolor sit. </ListTitle>
+                        <LI>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore in cum architecto illo similique commodi tenetur perspiciatis dignissimos eligendi non!
+    
+                    </LI>
+                        <LI>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore in cum architecto illo similique commodi tenetur perspiciatis dignissimos eligendi non!
+    
+                    </LI>
+                        <LI>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore in cum architecto illo similique commodi tenetur perspiciatis dignissimos eligendi non!
+    
+                    </LI>
+                        <LI>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore in cum architecto illo similique commodi tenetur perspiciatis dignissimos eligendi non!
+    
+                    </LI>
+                </OL>
 
-                    </li>
-                    <li>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore in cum architecto illo similique commodi tenetur perspiciatis dignissimos eligendi non!
+                    <UL>
+                    <ListTitle> Lorem, ipsum dolor sit amet consectetur adipisicing elit. </ListTitle>
+                        <LI>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore in cum architecto illo similique commodi tenetur perspiciatis dignissimos eligendi non!
+    
+                    </LI>
+                        <LI>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore in cum architecto illo similique commodi tenetur perspiciatis dignissimos eligendi non!
+    
+                    </LI>
+                        <LI>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore in cum architecto illo similique commodi tenetur perspiciatis dignissimos eligendi non!
+    
+                    </LI>
+                        <LI>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore in cum architecto illo similique commodi tenetur perspiciatis dignissimos eligendi non!
+    
+                    </LI>
+                    </UL>
+<P>
 
-                    </li>
-                    <li>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore in cum architecto illo similique commodi tenetur perspiciatis dignissimos eligendi non!
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quas incidunt mollitia explicabo, quo perferendis maiores voluptate amet vitae cumque odit facere. Excepturi nam debitis sed. Fuga, explicabo nulla maxime ipsum omnis itaque et nemo molestias excepturi enim ullam, ut, unde dicta veritatis illum maiores animi laboriosam praesentium magnam molestiae! Accusantium iste necessitatibus ex enim sequi quae velit. Deserunt sunt iste quos odio obcaecati consequatur fugit! Natus inventore incidunt maiores facere corporis! Reprehenderit quaerat corporis consequuntur reiciendis qui numquam beatae dicta eos officia, magnam tempore, expedita deserunt magni dolor ipsa perferendis doloribus sequi harum incidunt debitis tenetur veritatis. Aut odio fugit atque reprehenderit magnam repellat ab quae porro optio? Odio earum ea, sint hic mollitia debitis pariatur, dolorem rerum, ex itaque est doloremque sequi cumque expedita velit tempore libero ratione ab culpa. Laborum, commodi at? Quae unde possimus sapiente quaerat atque blanditiis dicta voluptatibus modi totam quod, iure pariatur, saepe praesentium a vero vel dolorem nobis voluptates expedita. Velit maiores recusandae praesentium inventore quae ad modi maxime. Incidunt a, voluptate maiores quos adipisci sint. Eligendi natus cum qui doloremque quod nemo eos saepe explicabo porro fuga, praesentium, reprehenderit itaque tempore! Ipsa magnam, commodi vel pariatur eveniet corporis accusantium aspernatur. Dolores.
+</P>
 
-                    </li>
-                    <li>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore in cum architecto illo similique commodi tenetur perspiciatis dignissimos eligendi non!
-
-                    </li>
-                </ol>
-
-                <ul>
-                    <li>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore in cum architecto illo similique commodi tenetur perspiciatis dignissimos eligendi non!
-
-                    </li>
-                    <li>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore in cum architecto illo similique commodi tenetur perspiciatis dignissimos eligendi non!
-
-                    </li>
-                    <li>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore in cum architecto illo similique commodi tenetur perspiciatis dignissimos eligendi non!
-
-                    </li>
-                    <li>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore in cum architecto illo similique commodi tenetur perspiciatis dignissimos eligendi non!
-
-                    </li>
-                </ul>
             </SupportCategoryWrapper>
 
         </SupportCategorySection>
-    )
-}
-
+            )
+        }
+        
 export default SupportCategory
