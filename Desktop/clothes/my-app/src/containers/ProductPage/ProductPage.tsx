69d2aed24f0bce65c80f3578c45dbb2609bcb783
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-08-27 22:14:22 +0200

    create product page container

---
+++ Desktop/clothes/my-app/src/containers/ProductPage/ProductPage.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+interface Iprops {
+
+}
+interface IState {
+
+}
+class ProductPage extends React.Component<Iprops, IState> {
+
+    render() {
+        return (
+            <div>
+                ProductPage
+            </div>
+        )
+    }
+
+}
+
+
+
+export default ProductPage
