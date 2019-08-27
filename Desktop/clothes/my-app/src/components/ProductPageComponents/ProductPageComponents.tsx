f085e0485cb5c3f5492966aa76bf89700d80888a
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-08-27 22:21:56 +0200

    create product page component

---
+++ Desktop/clothes/my-app/src/components/ProductPageComponents/ProductPageComponents.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Styled from 'styled-components'
+
+const ProductPageComponent: React.SFC = () => {
+
+    return (
+        <div>
+            ProductPageComponent
+        </div>
+    )
+}
+
+export default ProductPageComponent
