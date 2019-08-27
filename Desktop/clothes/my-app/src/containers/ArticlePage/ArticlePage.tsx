83ef98c6bb6de2a441df4db389490eb32de8974b
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-08-27 21:02:47 +0200

    create article page container

---
+++ Desktop/clothes/my-app/src/containers/ArticlePage/ArticlePage.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+interface Iprops {
+
+}
+interface IState {
+
+}
+class ArticlePage extends React.Component<Iprops, IState> {
+
+    render() {
+        return (
+            <div>
+                ArticlePage
+            </div>
+        )
+    }
+
+}
+
+
+
+export default ArticlePage
