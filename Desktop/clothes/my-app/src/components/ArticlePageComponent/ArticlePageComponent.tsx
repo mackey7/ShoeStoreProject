a3fc550e3d2da3d429d7962ccec72b85ff676fc4
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-08-27 21:04:27 +0200

    create article page component

---
+++ Desktop/clothes/my-app/src/components/ArticlePageComponent/ArticlePageComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Styled from 'styled-components'
+
+const ArticlePageComponent: React.SFC = () => {
+
+    return (
+        <div>
+            ArticlePageComponent
+        </div>
+    )
+}
+
+export default ArticlePageComponent
