a54188bfa03f4a5b2988fd7d0ef609f186a034c8
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-08-28 23:33:52 +0200

    create cart page container

---
+++ Desktop/clothes/my-app/src/containers/CartPage/CartPage.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+
+interface Iprops {
+
+}
+interface IState {
+
+}
+class CartPage extends React.Component<Iprops, IState> {
+
+    render() {
+        return (
+            <div>
+                CartPage
+
+            </div>
+        )
+    }
+
+}
+
+
+
+export default CartPage
