8417e9a73d8825de83bdd1c32d697c43bc227daa
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-08-28 23:12:11 +0200

    add styles

--- Desktop/clothes/my-app/src/components/SecondMenu/FavouriteSection.tsx
+++ Desktop/clothes/my-app/src/components/SecondMenu/FavouriteSection.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-
 import Styled from 'styled-components'
+
+const PIC = "https://n1.sdlcdn.com/imgs/h/1/7/Wdl-Sneakers-Red-Casual-Shoes-SDL039762466-1-2a564.jpg"
 
 const Section = Styled.div`
 padding:10px;
@@ -25,16 +26,48 @@ top:45px;
 left:-80px;
 background:#fff;
 width:300px;
-border:1px solid red;
+border:2px solid #9a9a9a;
 display: ${(props: any) => (props.isOpen ? "block" : "none")};
 `
-const ProductItem = Styled.div``
-const ProductImg = Styled.img``
-const ProductName = Styled.p``
-const DeleteProduct = Styled.button``
-const More = Styled.button`
-width: 100 %;
+const ProductItem = Styled.div`
+display:flex;
+align-items:center;
+justify-content: space-around;
+border-bottom:2px solid #9a9a9a;
+`
+const ProductImg = Styled.img`
+height:50px;
+width:50px;
+`
+const ProductName = Styled.p`
+
+`
+const DeleteProduct = Styled.button`
+background:inherit;
+border:0;
+font-size:20px;
 cursor: pointer;
+font-weight:400;
+    i{
+        color:red;
+            &:hover{
+            color:#ea0f0f;
+            }
+}
+`
+const More = Styled.button`
+width: 100%;
+padding:8px 0px;
+background:#da2121;
+font-size:18px;
+cursor: pointer;
+border:0px;
+font-weight:600;
+color:#fff;
+&:hover{
+
+    background:#ea0f0f;
+}
 `
 interface Iprops {
 
@@ -61,9 +94,6 @@ class FavouriteSection extends React.Component<Iprops, IState> {
 
     }
     render() {
-
-
-
         return (
             <Section>
                 <div onClick={this.SwitchIsOpen}>
@@ -72,9 +102,31 @@ class FavouriteSection extends React.Component<Iprops, IState> {
                 </div>
                 <Box isOpen={this.state.isOpen} >
                     <ProductItem>
-                        <ProductImg />
-                        <ProductName />
-                        <DeleteProduct />
+                        <ProductImg src={PIC} />
+                        <ProductName >
+                            Wdl Sneakers Red Casual Shoes
+                        </ProductName >
+                        <DeleteProduct >
+                            <i className="fas fa-trash-alt"></i>
+                        </DeleteProduct >
+                    </ProductItem>
+                    <ProductItem>
+                        <ProductImg src={PIC} />
+                        <ProductName >
+                            Wdl Sneakers Red Casual Shoes
+                        </ProductName >
+                        <DeleteProduct >
+                            <i className="fas fa-trash-alt"></i>
+                        </DeleteProduct >
+                    </ProductItem>
+                    <ProductItem>
+                        <ProductImg src={PIC} />
+                        <ProductName >
+                            Wdl Sneakers Red Casual Shoes
+                        </ProductName >
+                        <DeleteProduct >
+                            <i className="fas fa-trash-alt"></i>
+                        </DeleteProduct >
                     </ProductItem>
                     <More>More</More>
                 </Box>
