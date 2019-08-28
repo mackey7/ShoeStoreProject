c64674e7d1c9f07d96b07240ac1fe4b707d2ed97
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-08-29 00:13:09 +0200

    create favourite page conatainer

---
+++ Desktop/clothes/my-app/src/containers/FavouritePage/FavouritePage.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+
+interface Iprops {
+
+}
+interface IState {
+
+}
+class FavouritePage extends React.Component<Iprops, IState> {
+
+    render() {
+        return (
+            <div>
+              FavouritePage
+  
+            </div>
+        )
+    }
+
+}
+
+
+
+export default FavouritePage
