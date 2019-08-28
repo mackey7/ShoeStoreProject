ce90f3ea9f09e9b4137dfa93f86b60e529a4782d
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-08-28 23:47:02 +0200

    create cart page component

---
+++ Desktop/clothes/my-app/src/components/CartPageComponent/CartPageComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Styled from 'styled-components'
+
+const CartPageComponent: React.SFC = () => {
+
+    return (
+        <div>
+            CartPageComponent
+        </div>
+    )
+}
+
+export default CartPageComponent
