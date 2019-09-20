a9c2ec27816da7533f4b18cba931218d3859a316
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-09-20 19:30:27 +0200

    fix search component

--- Desktop/clothes/my-app/src/containers/SearchContainer/SearchContainer.tsx
+++ Desktop/clothes/my-app/src/containers/SearchContainer/SearchContainer.tsx
@@ -1,61 +1,74 @@
-import React from 'react'
-import SearchModal from './../../components/FirstMenu/SearchModal'
-import { fetchProductsToSearchSucces, searchProductsInProducts } from '../../actions/products'
+import React from "react";
+import SearchModal from "./../../components/FirstMenu/SearchModal";
+import {
+    fetchProductsToSearchSucces
+} from "../../actions/products";
 import { connect } from "react-redux";
-import searchProducts from '../../Utils/searchProducts'
 
 
 interface Iprops {
     products: any;
     fetchProductsToSearchSucces: any;
     switchOpenSearch: any;
-    searchProductsInProducts: any;
-
+    searchProductsInProducts?: any;
 }
 
-
-interface IState {
-
-}
+interface IState { }
 class SeaarchContainer extends React.Component<Iprops, IState> {
     state = {
-        inputValue: ''
-    }
+        inputValue: "",
+        searchedProducts: []
+    };
     handleChange = (e: any) => {
+        if (e.target.value === "") {
+            return this.setState({
+                searchedProducts: [],
+                inputValue: ""
+            });
+        }
+
+        this.setState({ inputValue: e.target.value });
+
+        const resultOfSearchingProducts = this.props.products.filter(
+            (product: any) => product.name.toLowerCase().includes(e.target.value.toLowerCase())
+        );
+
         this.setState({
-            inputValue: e.target.value
+            searchedProducts: resultOfSearchingProducts
         });
-        this.props.searchProductsInProducts(this.state.inputValue)
-    }
+    };
     componentDidMount() {
         this.props.fetchProductsToSearchSucces();
     }
     render() {
-        { console.log(this.state.inputValue) }
-        const { products, switchOpenSearch } = this.props
+        const { switchOpenSearch } = this.props;
         return (
             <div>
-                <SearchModal data={products} switchOpenSearch={switchOpenSearch} onChangeInput={this.handleChange} inputValue={this.state.inputValue} />
-
-            </div >
-        )
+                <SearchModal
+                    data={this.state.searchedProducts}
+                    switchOpenSearch={switchOpenSearch}
+                    onChangeInput={this.handleChange}
+                    inputValue={this.state.inputValue}
+                />
+            </div>
+        );
     }
-
 }
 
 const mapDispatchToProps = (dispatch: any) => {
-
     return {
         fetchProductsToSearchSucces: () => dispatch(fetchProductsToSearchSucces()),
-        searchProductsInProducts: (e: any) => dispatch(searchProductsInProducts(e)),
-    }
-}
+
+    };
+};
 
 const mapStateToProps = (state: any) => {
     return {
-        products: searchProducts(state)
+        products: state.products.products
+    };
+};
 
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(SeaarchContainer) 
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(SeaarchContainer);
