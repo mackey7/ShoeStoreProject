81d11b22663b09cb783123b52756a9136b247035
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-11-12 22:22:09 +0100

    build project

---
+++ Desktop/clothes/my-app/build/precache-manifest.0b5c2d6f3b802db8471d4c5fe4f04365.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9cd5b4cdaa537697a6d4",
+    "url": "/static/js/main.9cd5b4cd.chunk.js"
+  },
+  {
+    "revision": "c44eac80c30908e3b316",
+    "url": "/static/js/1.c44eac80.chunk.js"
+  },
+  {
+    "revision": "9cd5b4cdaa537697a6d4",
+    "url": "/static/css/main.8e35afec.chunk.css"
+  },
+  {
+    "revision": "c44eac80c30908e3b316",
+    "url": "/static/css/1.393806ae.chunk.css"
+  },
+  {
+    "revision": "6394710a01f0d12d1ecbf6e5896bea3e",
+    "url": "/index.html"
+  }
+];
